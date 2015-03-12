fa4c918199a7bf984d66bbe0e634b8ab5486e85c
Ubuntu <ubuntu@ip-172-31-38-131.us-west-2.compute.internal>
2015-03-12 04:28:22 +0000

    add gitignore

--- server.js
+++ server.js
@@ -7,15 +7,37 @@ var WebSocketServer = require('ws').Server,
 app.use(express.static(__dirname + '/static'));
 
 var clients = [];
+var players = [];
+
 wss.on('connection', function connection(ws) {
+  console.log('conn');
   clients.push(ws);
+
+  for (var i = 0; i < players.length; i++) {
+    if (!ws.playerID || players[i] != ws.playerID) {
+      ws.send(JSON.stringify({type: 'new_player', position: [0, 0, 0], network_id: players[i]}));
+    }  
+  };
+
   ws.on('message', function incoming(message, type, tgt) {
-   console.log('received: ', message, 'from client:', ws.upgradeReq.client);
-   for (var i = 0; i < clients.length;i++) {
+    console.log('received: ', message);
+    var parsed = JSON.parse(message);
+    if (parsed.type = 'new_player') {
+      ws.playerID = parsed.network_id;
+      players.push(parsed.network_id);
+    } 
+    for (var i = 0; i < clients.length;i++) {
       if (clients[i] != ws) {
        clients[i].send(message);
-     }
+      }
     }
   });
-  ws.send('something');
+  ws.on('close', function() {
+    for (var i = 0; i < clients.length;i++) {
+      if (clients[i] == ws) {
+        clients.splice(i, 1);
+      };
+    };
+    players.splice(players.indexOf(ws.playerID), 1);
+  });
 });
